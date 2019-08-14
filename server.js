e45971234b4fdf320de6f0058c006329c78960ae
Geoff Hankerson <ghankerson@mor.org>
2019-08-14 15:44:14 -0500

    no express

--- server.js
+++ server.js
@@ -1,57 +1,58 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const fetch = require('node-fetch')
+const http = require('http');
+const port = 3100;
+const fetch = require('node-fetch');
 
 const pageSize = 100;
-  // RSS feeds for collections
-  app.get(`/sitemap`, (req, res) => {
-    res.header('Content-Type', 'text/xml');
-    let xml = '<?xml version="1.0" encoding="UTF-8"?>';
-    const query = JSON.stringify({
-      query: `
+
+const requestHandler = (req, res) => {
+  res.setHeader('Content-Type', 'text/xml');
+  let xml = '<?xml version="1.0" encoding="UTF-8"?>';
+  const query = JSON.stringify({
+    query: `
         {
           sitemap: contentList(contentAreaSlug: "mprnews", contentTypes: [STORIES, PAGES, EPISODES], pageSize:${pageSize}) {
             totalPages
           }
         }
       `
-    });
-    xml +=
-      '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`';
-    const fetchFeedData = async (query) => {
-      return await fetch("https://cmsproxy.publicradio.org/api/v1/graphql", {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: query
-      })
-        .then((response) => {
-          return response.json();
-        })
-        .then((response) => {
-          return response;
-        })
-        .catch((err) => {
-          // eslint-disable-next-line
-          console.error('Error: ', err);
-        });
-    };
-    const queryRes = fetchFeedData(query);
-    queryRes.then(() => {
-      // graphql limited to 100 for now
-      // when that limit is solved,  use  results.data.sitemap.totalPages
-      for (let page = 1; page <= 100; page++) {
-        xml += '<sitemap>';
-        xml += `<loc>${req.protocol}://${req.headers.host}/sitemap/urlset/${page}</loc>`;
-        xml += '</sitemap>';
-      }
-      xml += '</sitemapindex>';
-      res.set('Cache-Control', 'public, max-age=1800'); // 30 minutes
-      res.send(xml);
-    });
   });
+  xml += '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`';
+  const fetchFeedData = (query) => {
+    return fetch('https://cmsproxy.publicradio.org/api/v1/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: query
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        return response;
+      })
+      .catch((err) => {
+        // eslint-disable-next-line
+        console.error('Error: ', err);
+      });
+  };
+  const queryRes = fetchFeedData(query);
+  queryRes.then(() => {
+    // graphql limited to 100 for now
+    // when that limit is solved,  use  results.data.sitemap.totalPages
+    for (let page = 1; page <= 100; page++) {
+      xml += '<sitemap>';
+      xml += `<loc>${req.protocol}://${req.headers.host}/sitemap/urlset/${page}</loc>`;
+      xml += '</sitemap>';
+    }
+    xml += '</sitemapindex>';
+    res.setHeader('Cache-Control', 'public, max-age=1800'); // 30 minutes
+    res.end(xml);
+  });
+};
 
+const server = http.createServer(requestHandler);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+server.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
