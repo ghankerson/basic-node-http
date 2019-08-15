8f03bd73ac0238732d11667db1d00abe7271fa30
Geoff Hankerson <ghankerson@mor.org>
2019-08-15 11:39:31 -0500

    refactor to try not to leak

--- server.js
+++ server.js
@@ -2,22 +2,22 @@ const http = require('http');
 const port = 3100;
 const fetch = require('node-fetch');
 
-const pageSize = 100;
-
-const requestHandler = (req, res) => {
-  res.setHeader('Content-Type', 'text/xml');
-  let xml = '<?xml version="1.0" encoding="UTF-8"?>';
-  const query = JSON.stringify({
-    query: `
-        {
-          sitemap: contentList(contentAreaSlug: "mprnews", contentTypes: [STORIES, PAGES, EPISODES], pageSize:${pageSize}) {
-            totalPages
-          }
-        }
-      `
-  });
-  xml += '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`';
-  const fetchFeedData = (query) => {
+const app = {
+  xmlbuilder: function(host) {
+    let xml = '<?xml version="1.0" encoding="UTF-8"?>';
+    xml +=
+      '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`';
+    // graphql limited to 100 for now
+    // when that limit is solved,  use  results.data.sitemap.totalPages
+    for (let page = 1; page <= 100; page++) {
+      xml += '<sitemap>';
+      xml += `<loc>http://${host}/sitemap/urlset/${page}</loc>`;
+      xml += '</sitemap>';
+    }
+    xml += '</sitemapindex>';
+    return xml;
+  },
+  makeQuery: function(query) {
     return fetch('https://cmsproxy.publicradio.org/api/v1/graphql', {
       method: 'POST',
       headers: {
@@ -35,21 +35,28 @@ const requestHandler = (req, res) => {
         // eslint-disable-next-line
         console.error('Error: ', err);
       });
-  };
-  const queryRes = fetchFeedData(query);
+  }
+};
+
+function requestHandler(req, res) {
+  const pageSize = 100;
+  const query = JSON.stringify({
+    query: `
+        {
+          sitemap: contentList(contentAreaSlug: "mprnews", contentTypes: [STORIES, PAGES, EPISODES], pageSize:${pageSize}) {
+            totalPages
+          }
+        }
+      `
+  });
+
+  const queryRes = app.makeQuery(query);
   queryRes.then(() => {
-    // graphql limited to 100 for now
-    // when that limit is solved,  use  results.data.sitemap.totalPages
-    for (let page = 1; page <= 100; page++) {
-      xml += '<sitemap>';
-      xml += `<loc>${req.protocol}://${req.headers.host}/sitemap/urlset/${page}</loc>`;
-      xml += '</sitemap>';
-    }
-    xml += '</sitemapindex>';
-    res.setHeader('Cache-Control', 'public, max-age=1800'); // 30 minutes
+    const xml = app.xmlbuilder(req.headers.host);
+    res.setHeader('Content-Type', 'text/xml');
     res.end(xml);
   });
-};
+}
 
 const server = http.createServer(requestHandler);
 
