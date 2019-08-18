cb718d463302340b40c086c4c08933eb2ae4b4c1
Geoff Hankerson <ghankerson@mor.org>
2019-08-18 15:30:13 -0500

    add https agent keep-alive

--- server.js
+++ server.js
@@ -1,44 +1,46 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const port = 3100;
 const fetch = require('node-fetch');
+const https = require('https');
+const agent = new https.Agent({ keepAlive: true });
 
-const app = {
-  xmlbuilder: function(host) {
-    let xml = '<?xml version="1.0" encoding="UTF-8"?>';
-    xml +=
-      '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`';
-    // graphql limited to 100 for now
-    // when that limit is solved,  use  results.data.sitemap.totalPages
-    for (let page = 1; page <= 100; page++) {
-      xml += '<sitemap>';
-      xml += `<loc>http://${host}/sitemap/urlset/${page}</loc>`;
-      xml += '</sitemap>';
+const wtf = function() {
+  return {
+    xmlbuilder: function(host) {
+      let xml = '<?xml version="1.0" encoding="UTF-8"?>';
+      xml +=
+        '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`';
+      // graphql limited to 100 for now
+      // when that limit is solved,  use  results.data.sitemap.totalPages
+      for (let page = 1; page <= 100; page++) {
+        xml += '<sitemap>';
+        xml += `<loc>http://${host}/sitemap/urlset/${page}</loc>`;
+        xml += '</sitemap>';
+      }
+      xml += '</sitemapindex>';
+      return xml;
+    },
+    makeQuery: async function() {
+      return await fetch('https://cms.splendidtable.org/api/home', {
+        method: 'GET',
+        agent: agent
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((response) => {
+          return response;
+        })
+        .catch((err) => {
+          // eslint-disable-next-line
+          console.error('Error: ', err);
+        });
     }
-    xml += '</sitemapindex>';
-    return xml;
-  },
-  makeQuery: function(query) {
-    return fetch('https://cmsproxy.publicradio.org/api/v1/graphql', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: query
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((response) => {
-        return response;
-      })
-      .catch((err) => {
-        // eslint-disable-next-line
-        console.error('Error: ', err);
-      });
-  }
+  };
 };
 
-function requestHandler(req, res) {
+app.get('/', (req, res) => {
   const pageSize = 100;
   const query = JSON.stringify({
     query: `
@@ -50,16 +52,12 @@ function requestHandler(req, res) {
       `
   });
 
-  const queryRes = app.makeQuery(query);
-  queryRes.then(() => {
-    const xml = app.xmlbuilder(req.headers.host);
-    res.setHeader('Content-Type', 'text/xml');
-    res.end(xml);
-  });
-}
+  let wtfReally = new wtf();
+  const queryRes = wtfReally.makeQuery(query);
+  const xml = wtfReally.xmlbuilder(req.headers.host);
+  res.setHeader('Content-Type', 'text/xml');
+  wtfReally = null;
+  res.end(xml);
+});
 
-const server = http.createServer(requestHandler);
-
-server.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`)
-);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
